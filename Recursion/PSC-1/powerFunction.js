0b9329ad9be05ca380b47ccc0a7397ed1ac4d76a
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-04-08 23:28:00 +0530

    Power Function

---
+++ Recursion/PSC-1/powerFunction.js
@@ -0,0 +1,13 @@
+function powerFunction (a,b){
+    if(b==0){
+        return 1
+    }
+    else{
+        return a * powerFunction(a, b-1)
+    }
+}
+
+let a = 2
+let b = 4
+
+console.log(powerFunction(a,b))
