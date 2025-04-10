ccac03553ac025cf8cafc77df016b9140473f27d
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-04-10 23:45:40 +0530

    Count Bits

--- Recursion/PSC-2/count-bits.js
+++ Recursion/PSC-2/count-bits.js
@@ -16,5 +16,20 @@
 // Output Description
 // For each test case, print the array.
 
+function runProgram(input) {
+    input = input.trim().split('\n');
+    let T = +input[0];
+    let line = 1;
 
+    for (let t = 0; t < T; t++) {
+        let n = +input[line++];
+        let ans = new Array(n + 1).fill(0);
+
+        for (let i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+
+        console.log(ans.join(' '));
+    }
+}
 
