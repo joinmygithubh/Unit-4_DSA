f3f97d2b9249bb8f9e4bf669be1545f11de343b2
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-04-16 22:36:08 +0530

    Merge Sort

---
+++ MergeSort/mergeSort.js
@@ -0,0 +1,38 @@
+function runProgram(input) {
+    input = input.trim().split("\n");
+     const n = +input[0];
+     const arr = input[1].trim().split(" ").map(Number);
+   
+     const sortedArr = mergeSort(arr);
+     console.log(sortedArr.join(" "));
+   }
+   
+   function mergeSort(arr) {
+     if (arr.length <= 1) return arr;
+   
+     const mid = Math.floor(arr.length / 2);
+     const left = mergeSort(arr.slice(0, mid));
+     const right = mergeSort(arr.slice(mid));
+   
+     return merge(left, right);
+   }
+   
+   function merge(left, right) {
+     const result = [];
+     let i = 0, j = 0;
+   
+     while (i < left.length && j < right.length) {
+       if (left[i] < right[j]) {
+         result.push(left[i]);
+         i++;
+       } else {
+         result.push(right[j]);
+         j++;
+       }
+     }
+   
+     while (i < left.length) result.push(left[i++]);
+     while (j < right.length) result.push(right[j++]);
+   
+     return result;
+   }
