1a5e29a87c05f52f1b814edc803cadfa499de255
Sameer Ahmed <sameerahmed1601@gmail.com>
2025-05-02 23:46:22 +0530

    Triangle or not

--- PSC on Abstract Binary Search/triangleOrNot.js
+++ PSC on Abstract Binary Search/triangleOrNot.js
@@ -1,7 +1,6 @@
 function runProgram(input) {
     let [a, b, c] = input.trim().split(" ").map(Number);
 
-    
     if (a + b > c && a + c > b && b + c > a) {
       console.log("Yes");
     } else {
